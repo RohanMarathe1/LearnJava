0ec6a2d60a7853ee779d518b207c569ebb9a015f
RohanMarathe1 <Rohan.Marathe1@education.nsw.gov.au>
2024-01-07 21:05:13 +1100

    Change swaping name to rohanmarathe

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         numberWar.sumOfEvenOdd();
 
         StringSwapper stringSwapper = new StringSwapper() ;
-        System.out.println(stringSwapper.swapFirstAndLast("ROHAN"));
+        System.out.println(stringSwapper.swapFirstAndLast("ROHANMARATHE"));
 
         Loop loop = new Loop() ;
         loop.replaceChar();
