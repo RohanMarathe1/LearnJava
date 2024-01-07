3ff66172048cded38ad1f536a833ad74d52fc646
RohanMarathe1 <Rohan.Marathe1@education.nsw.gov.au>
2024-01-07 20:57:56 +1100

    remove spaces

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args){
-
         Looping looping = new Looping() ;
         looping.replaceWithLoop();
 
@@ -13,6 +12,5 @@ public class Main {
 
         Loop loop = new Loop() ;
         loop.replaceChar();
-
     }
 }
